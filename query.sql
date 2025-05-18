5ae234e5df0ebadf698df732b1566c044fd848d5
SteveDo23-meet <109904642+SteveDo23-meet@users.noreply.github.com>
2025-05-18 13:10:13 +0300

    Delete query.sql

--- query.sql
+++
@@ -1,8 +0,0 @@
-SELECT c.CustomerID, c.FirstName, c.LastName, SUM(oi.Quantity * oi.UnitPrice) AS TotalSpent
-FROM customers c
-JOIN orders o ON c.CustomerID = o.CustomerID
-JOIN orderitems oi ON o.OrderID = oi.OrderID
-GROUP BY c.CustomerID
-ORDER BY TotalSpent DESC;
-
-
